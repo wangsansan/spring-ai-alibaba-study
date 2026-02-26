6009c5ce127284dd88bb1e28721a1b199e52951e
wangchunsheng24 <wangchunsheng24@jd.com>
2026-02-26 10:18:57 +0800

    optimize code

--- src/main/java/com/wcs/ai/alibaba/utils/Constants.java
+++ src/main/java/com/wcs/ai/alibaba/utils/Constants.java
@@ -12,16 +12,16 @@ public interface Constants {
             
                 If a user asks you for the weather, make sure you know the location.
                 If you can tell from the question that they mean wherever they are,
-                use the get_user_location tool to find their location.
+                use the getUserLocation tool to find their location.
             """;
 
     String CUSTOM_SCHEMA = """
-    请按照以下JSON格式输出：
-    {
-        "title": "标题",
-        "content": "内容",
-        "style": "风格"
-    }
-    """;
+            请按照以下JSON格式输出：
+            {
+                "title": "标题",
+                "content": "内容",
+                "style": "风格"
+            }
+            """;
 
 }
